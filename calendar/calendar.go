7e749531b539091044fdf7337e9559f24a271f82
Abhijeet Rastogi <abhijeet.1989@gmail.com>
2023-08-18 14:53:54 -0700

    refactor
    r# Please enter the commit message for your changes. Lines starting

--- calendar/calendar.go
+++ calendar/calendar.go
@@ -1,6 +1,14 @@
 package calendar
 
+import "time"
+
 type Calendar interface {
 	GetEvents()
 	PutEvents()
+}
+
+type Event struct {
+	Title       string
+	Start, Stop time.Time
+	UID         string
 }
